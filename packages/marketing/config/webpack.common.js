6c4759ab6cab4ac88d28543bb59c53f835c1ee77
Joaquin <jcarbone@cegid.com>
2023-07-26 11:10:16 +0200

    Initial commit

---
+++ packages/marketing/config/webpack.common.js
@@ -0,0 +1,17 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader', // transpiles modern JS to ES5
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'], // transpiles modern JS to ES5
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
+    ],
+  },
+}
